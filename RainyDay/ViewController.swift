e4df114c7420e423f9553529b6635f4284561dd9
Mthokozisi Malaza <mthokozisi.malaza@absa.africa>
2023-10-03 17:13:09 +0200

    Initial Commit

---
+++ RainyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RainyDay
+//
+//  Created by Mthokozisi Malaza on 2023/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
